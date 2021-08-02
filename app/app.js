f34a84e592f113a13ee6479d68271df9d3d4bdd0
Jeongjewww <gwmelody99@gmail.com>
2021-08-02 16:52:06 +0900

    optimizing folders

---
+++ app/app.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const PORT = 3000;
+
+// 모듈
+const express = require("express");
+const app = express();
+
+// 라우팅
+const home = require("./src/routes/home");  // index.js를 읽음.
+
+// 앱 세팅
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+app.use("/", home); // use: 미들웨어 등록 메소드
+
+module.exports = app;
