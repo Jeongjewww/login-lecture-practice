bba5f6d6eb933bbe6fbbf70ab75f84ac505e4f38
Jeongjewww <gwmelody99@gmail.com>
2021-08-03 14:56:03 +0900

    apply nodemon

--- app/app.js
+++ app/app.js
@@ -12,6 +12,7 @@ const home = require("./src/routes/home");  // index.js를 읽음.
 // 앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", home); // use: 미들웨어 등록 메소드
 
