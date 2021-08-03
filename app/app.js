ee4ad7e673563c1f16b1e5027e31658367f08961
Jeongjewww <gwmelody99@gmail.com>
2021-08-03 21:24:36 +0900

    Login fun at Backend

--- app/app.js
+++ app/app.js
@@ -13,6 +13,9 @@ const home = require("./src/routes/home");  // index.js를 읽음.
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
+app.use(express.json());
+//url을 통해 전달되는 데이터에 한글, 공백 같은 문자가 제대로 인식되지 않는 문제 해결
+app.use(express.urlencoded({ extended: true}));
 
 app.use("/", home); // use: 미들웨어 등록 메소드
 
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,40 @@
 "use strict";
 
-const hello = (req, res) => {
-    res.render("home/index");
+const output = {
+    hello: (req, res) => {
+        res.render("home/index");
+    },
+    login: (req, res) => {
+        res.render("home/login");
+    }
 };
 
-const login = (req, res) => {
-    res.render("home/login");
+const users = {
+    id: ["woorimIT", "gwmelody"],
+    psword: ["123", "1341"]
 };
+
+const process = {
+    login: (req, res) => {
+        const id = req.body.id,
+        psword = req.body.psword;
+
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.psword[idx] === psword){
+                return res.json({
+                    success: true
+                });
+            }
+        }
+        return res.json({
+            success:false,
+            msg: "로그인에 실패하였습니다"
+        });
+    }
+}
 
 module.exports = {
-    hello,
-    login
+    output, 
+    process
 };
